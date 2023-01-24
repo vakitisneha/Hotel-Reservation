482f84851511772f500bcefe7490f847f3ad5cf9
sneha123-123 <vakitisneha1234@gmail.com>
2023-01-24 12:39:30 +0530

    Distributors Api

---
+++ Websales Distributorrs Api/DistributorsProjectController.cs
@@ -0,0 +1,92 @@
+﻿using samplewebapi.Models;
+using samplewebapi.rep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace samplewebapi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DistributorsProjectController : ApiController
+    {
+        public readonly IDistributorsProject IdistributorsProject;
+        public DistributorsProjectController(IDistributorsProject idistributors)
+        {
+            this.IdistributorsProject = idistributors;
+        }
+        [Route("api/DistributorsProject/GetallDetails")]
+        [HttpGet]
+        public List<DistributorsProjectModel> GetallDetails()
+        {
+            var ret = IdistributorsProject.GetallDetails();
+            return ret;
+        }
+       [Route("api/DistributorsProject/insert")]
+       [HttpPost]
+        public HttpResponseMessage insertdetails(DistributorsProjectModel i)
+        {
+            var ret = IdistributorsProject.insertdetails(i);
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+        }
+        [Route("api/DistributorsProject/GetaddressDetails")]
+        [HttpGet]
+        public List<DistributorsProjectModel> GetaddressDetails()
+        {
+            var ret = IdistributorsProject.GetaddressDetails();
+            return ret;
+        }
+        [Route("api/DistributorsProject/DeleteaddressDetails/{MobileNumber}")]
+        [HttpDelete]
+        
+        public HttpResponseMessage DeleteadressDetails(int MobileNumber)
+        {
+            var ret = IdistributorsProject.DeleteaddressDetails(MobileNumber);
+
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+        }
+
+        [Route("api/DistributorsProject/insertpayment")]
+        [HttpPost]
+        public HttpResponseMessage insertpaymentdetails(DistributorsProjectModel i)
+        {
+
+            var ret = IdistributorsProject.insertpaymentdetails(i);
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+
+        }
+        [Route("api/DistributorsProject/delete/{price}")]
+        [HttpDelete]
+        public HttpResponseMessage DeleteDetails(int price)
+        {
+            var ret = IdistributorsProject.DeleteDetails(price);
+
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+        }
+        [Route("api/DistributorsProject/insertemp")]
+        [HttpPost]
+        public HttpResponseMessage insertEmpDetails(RegistrationModel pro)
+        {
+
+            var ret = IdistributorsProject.insertemp(pro);
+            return Request.CreateResponse(HttpStatusCode.OK, ret);
+
+        }
+
+    }
+}
+
+//[Route("api/DistributorsProject/joins")]
+//[HttpGet]
+//public IEnumerable<DistributorsProjectModel> Getjoins()
+//{
+
+
+//    return IdistributorsProject.Getjoins().ToList();
+
+
+
+//}
---
+++ Websales Distributorrs Api/DistributorsProjectModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace samplewebapi.Models
+{
+    public class DistributorsProjectModel
+    {
+        public string productName { get; set; }
+        public string ProductId { get; set; }
+        public string image { get; set; }
+        public Nullable<decimal> price { get; set; }
+        public string Description { get; set; }
+        //public string prodId { get; set; }
+        //public string prodName { get; set; }
+        //public string prodimage { get; set; }
+        //public string prodDescription { get; set; }
+        //public Nullable<decimal> prodprice { get; set; }
+        //public Nullable<int> quantity { get; set; }
+        //public Nullable<int> total { get; set; }
+
+        public string FullName { get; set; }
+        public long MobileNumber { get; set; }
+        public Nullable<long> PinCode { get; set; }
+        public string FlatNO { get; set; }
+        public string BuildingName { get; set; }
+        public string Street { get; set; }
+        public string Village { get; set; }
+        public string City { get; set; }
+        public string states { get; set; }
+
+        public string Name { get; set; }
+        public long AccountNO { get; set; }
+        public Nullable<long> CVV { get; set; }
+        //public Nullable<int> Quantity { get; set; }
+        //public Nullable<int> Total { get; set; }
+
+    }
+    public class ProfileModel
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public Nullable<System.DateTime> Dob { get; set; }
+        public string ContactNO { get; set; }
+        public string Address { get; set; }
+        public Nullable<int> PANNo { get; set; }
+        public Nullable<int> BankIFCSCode { get; set; }
+        public int BankAccountNo { get; set; }
+    }
+        public class RegistrationModel
+        {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public Nullable<System.DateTime> Dob { get; set; }
+        public string ContactNO { get; set; }
+        public string Address { get; set; }
+        public Nullable<int> PANNo { get; set; }
+        public Nullable<int> BankIFCSCode { get; set; }
+        public int BankAccountNo { get; set; }
+    }
+    
+}
